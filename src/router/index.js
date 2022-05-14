2d30e5c0c75d910102e0d72998b9ba9e710b6173
koncetaint zheng <1941206108@qq.com>
2022-05-14 12:28:19 +0800

    用户注册

--- src/router/index.js
+++ src/router/index.js
@@ -7,8 +7,26 @@ const routes = [
     {
         path: '/',
         name: 'home',
-        component: () => import('@/views/HomeView.vue')
+        component: () => import('@/views/HomeView.vue'),
+        meta: { title: '首页' }
     },
+    {
+      path:'/register',
+      name:'register',
+      component: () => import('@/views/auth/Register'),
+      meta: { title: '注册'}
+    },
+    {
+      path:'/404',
+      name:'404',
+      component: () => import('@/views/error/404'),
+      meta: { title: '404-NotFound'}
+    },
+    {
+      path: '*',
+      redirect: '/404',
+      hidden: true
+    }
 
 ]
 
--- src/views/card/Promotion.vue
+++ src/views/card/Promotion.vue
@@ -5,15 +5,35 @@
               <span>推广</span>
           </div>
           <div>
-              推广body
+            <p v-for="(item,index) in list" :key="index" class="block">
+                <a :href="item.link" target="_blank">{{item.title}}</a>
+            </p>
           </div>
       </el-card>
   </div>
 </template>
 
 <script>
+import { getList } from '@/api/promote'
 export default {
-   name:'Promotion'
+   name:'Promotion',
+   data(){
+       return{
+           list:[]
+       } 
+   },
+   methods:{
+       async fetchList(){
+           getList().then( response => {
+               const { data } = response
+               this.list = data
+           })
+       }
+   },
+   created(){
+       this.fetchList()
+       console.log(this.list)
+   }
 }
 </script>
 
