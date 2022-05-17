f9e07b5d2807cd274ab0437c89de862038e9eb7f
koncetaint zheng <1941206108@qq.com>
2022-05-17 09:59:34 +0800

    帖子列表展示

--- src/main.js
+++ src/main.js
@@ -13,6 +13,19 @@ import '@/assets/app.css'
 
 import '@/permission'
 
+import relativeTime from 'dayjs/plugin/relativeTime';
+// 国际化
+import 'dayjs/locale/zh-cn'
+const dayjs = require('dayjs');
+
+// 相对时间插件
+dayjs.extend(relativeTime)
+
+dayjs.locale('zh-cn') // use locale globally
+dayjs().locale('zh-cn').format() // use locale in a specific instance
+
+Vue.prototype.dayjs = dayjs;//可以全局使用dayjs
+
 Vue.use(Buefy)
 Vue.use(ElementUI);
 Vue.config.productionTip = false
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer:{
+    proxy:"http://localhost:8088"   //之所以为5000，是因为目标服务器端口是在5000，代理是开在本服务器的，只需目的地
+  }      
 })
